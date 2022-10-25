d992f6b5eda1d41cf5198f024b8fb2e067ec49f6
bohdan-voit <113583224+bohdan-voit@users.noreply.github.com>
2022-10-25 09:59:05 +0300

    new exception  to child class

--- Animals/src/Bird.java
+++ Animals/src/Bird.java
@@ -1,9 +1,16 @@
 public class Bird extends Animal implements CanRun,Flyable {
+
+    @Override
+    public void massofanimalinpounds() throws Exception {
+        super.massofanimalinpounds();
+    }
+
     @Override
     public void run() {
         System.out.println("Птах біжить ");
 
     }
+
 
     public  void eat () {
         System.out.println("Пташиний корм");
